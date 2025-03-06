ed17e66e25e56d5d0394a2c109679ce0042dcd3b
bhogan12 <benhogan12@gmail.com>
2025-03-06 11:41:18 -0500

    deployment

--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
 import os
 import shutil
+import sys
 from web_generation import static_to_public, generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 tempalte_path = "./template.html"
+default_basepath = "/"
 
 def main():
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     print("Deleting...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -15,7 +21,7 @@ def main():
     print("Copying...")
     static_to_public(dir_path_static, dir_path_public)
 
-    generate_pages_recursive(dir_path_content, tempalte_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, tempalte_path, dir_path_public, basepath)
 
     
     
--- src/web_generation.py
+++ src/web_generation.py
@@ -17,7 +17,7 @@ def static_to_public(source_dir, target_dir):
             static_to_public(src, tgt)
     
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}.")
 
     with open(from_path) as file:
@@ -30,6 +30,8 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(from_md)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", md_html)
+    template = template.replace('href="/', 'href="' + basepath)
+    template = template.replace('src="/', 'src="' + basepath)
 
     if not os.path.exists(os.path.dirname(dest_path)):
         os.makedirs(os.path.dirname(dest_path), exist_ok=True)
@@ -37,12 +39,12 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, 'w') as file:
         file.write(template)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
